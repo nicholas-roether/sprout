6b8cff962c8e4cebf06b0ed93be83e570738bc80
Nicholas <nicholas.roether@t-online.de>
2022-10-27 19:05:53 +0200

    Add new grammar implementation

--- src/grammar.rs
+++ src/grammar.rs
@@ -1,12 +1,194 @@
-enum GrammarItemName<ProcName, TokenName> {
-	Terminal(TokenName),
-	NonTerminal(ProcName)
+use std::fmt;
+
+use trees::Tree;
+
+use crate::{tokenize::Token, fragment::{Fragment, SequenceView}};
+
+enum GrammarItemName<PN, TN> {
+	Terminal(TN),
+	NonTerminal(PN)
 }
 
-trait GrammarPart {
-	
+impl<PN: fmt::Display, TN: fmt::Display> fmt::Display for GrammarItemName<PN, TN> {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		match self {
+			Self::Terminal(token_name) => write!(f, "{}", token_name),
+			Self::NonTerminal(proc_name) => write!(f, "{}", proc_name)
+		}
+	}
 }
 
-struct GrammarProdedure {
-	
+enum GrammarTreeNode<PN, TN> {
+	Proc(PN),
+	Token(Token<TN>)
+}
+
+struct GrammarTreeBuilder<'a, PN, TN> {
+	grammar: &'a Grammar<'a, PN, TN>,
+	tree_stack: Vec<Tree<GrammarTreeNode<PN, TN>>>,
+	result: Result<Tree<GrammarTreeNode<PN, TN>>, String>
+}
+
+impl<'a, PN, TN> GrammarTreeBuilder<'a, PN, TN> {
+	fn new(grammar: &'a Grammar<'a, PN, TN>) -> Self {
+		GrammarTreeBuilder { grammar, tree_stack: vec![], result: Err(String::from("Syntax tree is incomplete!")) }
+	}
+
+	fn current_proc_mut<'b>(&'b mut self) -> &'b mut Tree<GrammarTreeNode<PN, TN>> {
+		self.tree_stack.last_mut().expect("Tried to push token, but no proc was pushed")
+	}
+
+	fn push_token(&mut self, token: Token<TN>) {
+		self.current_proc_mut().push_back(Tree::new(GrammarTreeNode::Token(token)));
+	}
+
+	fn push_proc(&mut self, name: PN) {
+		self.tree_stack.push(Tree::new(GrammarTreeNode::Proc(name)));
+	}
+
+	fn pop_proc(&mut self) {
+		if self.tree_stack.is_empty() { return; }
+		let popped_proc = self.tree_stack.pop().unwrap();
+		if self.tree_stack.is_empty() {
+			self.result = Ok(popped_proc);
+		} else {
+			self.current_proc_mut().push_back(popped_proc);
+		}
+	}
+}
+
+pub struct GrammarTokenFragment<TN> {
+	name: TN
+}
+
+impl<TN> GrammarTokenFragment<TN> {
+	pub fn new(name: TN) -> Self {
+		GrammarTokenFragment { name }
+	}
+}
+
+impl<'a, PN, TN: Eq + Copy + fmt::Display> Fragment<Token<TN>, GrammarTreeBuilder<'a, PN, TN>> for GrammarTokenFragment<TN> {
+	fn compare(
+		&self,
+		view: &mut SequenceView<Token<TN>>,
+		acc: &mut GrammarTreeBuilder<'a, PN, TN>
+	) -> Result<(), String> {
+		if view.items().is_empty() {
+			return Err(format!("Unexpected end of file; expected {}", self.name))
+		}
+		let next_token = view.items().first().unwrap();
+		if next_token.name != self.name {
+			return Err(format!("Expected {}, got \"{}\"", self.name, next_token.str));
+		}
+		view.advance(1);
+		acc.push_token(next_token.clone());
+		Ok(())
+	}
+}
+
+pub struct GrammarProcFragment<PN> {
+	name: PN
+}
+
+impl<PN> GrammarProcFragment<PN> {
+	pub fn new(name: PN) -> Self {
+		GrammarProcFragment { name }
+	}
+}
+
+impl<'a, PN: Eq + Copy + fmt::Display, TN> Fragment<Token<TN>, GrammarTreeBuilder<'a, PN, TN>> for GrammarProcFragment<PN> {
+	fn compare(
+		&self,
+		view: &mut SequenceView<Token<TN>>,
+		acc: &mut GrammarTreeBuilder<'a, PN, TN>
+	) -> Result<(), String> {
+		if view.items().is_empty() {
+			return Err(format!("Unexpected end of file; expected {}", self.name))
+		}
+		acc.push_proc(self.name);
+		acc.grammar.compare_proc(self.name, view, acc)?;
+		acc.pop_proc();
+		Ok(())
+	}
+}
+
+struct GrammarProc<'a, PN, TN> {
+	name: PN,
+	fragment: Box<dyn Fragment<Token<TN>, GrammarTreeBuilder<'a, PN, TN>>>
+}
+
+impl<'a, PN, TN> GrammarProc<'a, PN, TN> {
+	fn new(name: PN, fragment: Box<dyn Fragment<Token<TN>, GrammarTreeBuilder<'a, PN, TN>>>) -> Self {
+		GrammarProc { name, fragment }
+	}
+}
+
+struct Grammar<'a, PN, TN> {
+	procs: Vec<GrammarProc<'a, PN, TN>>
+}
+
+impl<'a, PN: Eq, TN> Grammar<'a, PN, TN> {
+	pub fn new(procs: Vec<GrammarProc<'a, PN, TN>>) -> Self {
+		Grammar { procs }
+	}
+
+	pub fn parse(&'a self, proc: PN, tokens: Vec<Token<TN>>) -> Result<Tree<GrammarTreeNode<PN, TN>>, String> {
+		let mut seq_view = SequenceView::new(&tokens);
+		let mut tree_builder: GrammarTreeBuilder<'a, PN, TN> = GrammarTreeBuilder::new(&self);
+		self.compare_proc(proc, &mut seq_view, &mut tree_builder)?;
+		tree_builder.result
+	}
+
+	fn compare_proc(
+		&self,
+		proc: PN,
+		view: &mut SequenceView<Token<TN>>,
+		acc: &mut GrammarTreeBuilder<'a, PN, TN>
+	) -> Result<(), String> {
+		let mut first_error: Option<String> = None;
+		for proc in self.procs.iter().filter(|p| p.name == proc) {
+			if let Err(msg) = proc.fragment.compare(view, acc) {
+				if first_error.is_none() {
+					first_error = Some(msg);
+				}
+			} else {
+				return Ok(())
+			}
+		}
+		Err(first_error.unwrap_or(String::from("Unexpected error during parsing")))
+	}
+}
+
+macro_rules! grammar_part_sequence {
+	($($part:tt),+) => {
+		crate::fragment::SequenceFragment::new(vec![
+			$(Box::new($part)),+
+		])
+	};
+	($part:expr) => {
+		$part
+	};
+}
+
+#[macro_export]
+macro_rules! grammar {
+	($($proc_name:expr => $($name:expr),+;)*) => {
+		crate::grammar::Grammar::new(vec![
+			$(crate::grammar::GrammarProc::new($proc_name, Box::new(grammar_part_sequence!($(($name)),+)))),*
+		])
+	};
+}
+
+#[macro_export]
+macro_rules! token {
+	($name:expr) => {
+		crate::grammar::GrammarTokenFragment::new($name)
+	};
+}
+
+#[macro_export]
+macro_rules! proc {
+	($name:expr) => {
+		crate::grammar::GrammarProcFragment::new($name)
+	};
 }
